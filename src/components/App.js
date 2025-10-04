b9c710b50ed186185f1333c36a07219eda4e27c6
bharti025 <20001003032bharti@dcrustm.org>
2025-10-05 00:29:33 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,15 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-       <Link to="/item1">Item 1</Link>
-        <Link to="/item2">Item 2</Link>
-         <Link to="/item3">Item 3</Link>
-   
+      <h1>Item List</h1>
+       <Link to="/items/1">Item 1</Link>
+        <Link to="/items/2">Item 2</Link>
+         <Link to="/items/3">Item 3</Link>
+
          <Routes>
-          <Route path="/item1" element={<Item1/>}/>
-          <Route path="/item2" element={<Item2/>}/>
-          <Route path="/item3" element={<Item3/>}/>
+          <Route path="/items/1" element={<Item1/>}/>
+          <Route path="/items/2" element={<Item2/>}/>
+          <Route path="/items/3" element={<Item3/>}/>
           </Routes>
  </div>
   )
